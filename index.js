6102c5203323623883c0a372307c4befc8e9064f
GkNdev <kreosus98@gmail.com>
2025-04-20 01:41:41 +0300

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
-    const res = await fetch("https://vavoo.to/play/1536730627/index.m3u8");
+    const response = await fetch("https://vavoo.to/play/1536730627/index.m3u8");
     res.redirect(res.location);
 });
 
