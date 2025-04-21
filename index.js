015c59eec28fcb18ff6405b56a83c86c85a90db7
GkNdev <kreosus98@gmail.com>
2025-04-21 23:23:50 +0300

    removed sozcu

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const puppeteer = require('puppeteer');
 
 const port = process.env.PORT || 3000;
 
@@ -9,7 +8,6 @@ app.get('/', (req, res) => {
 });
 
 app.get('/gkn/:channel', async (req, res) => {
-  const channel = req.params.channel;
 
   if (channel === 'nowtv') {
     const response = await fetch('https://www.nowtv.com.tr/canli-yayin');
@@ -28,53 +26,6 @@ app.get('/gkn/:channel', async (req, res) => {
     } else {
       res.status(404).json({ error: 'HLS URL Bulunamadı , goflex telegramdan admine bildirebilirsiniz.' });
     }
-  } else if (channel === "sozcu") {
-    try {
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-
-      // Set headers
-      await page.setExtraHTTPHeaders({
-        "Referer": "https://www.ecanlitvizle.app/"
-      });
-
-      await page.goto('https://www.ecanlitvizle.app/embed.php?kanal=szc-tv-sozcu-televizyonu-canli-izle');
-
-      await page.evaluate(() => {
-        const element = document.querySelector(".jw-icon-display");
-        if (element) {
-          element.click();
-        }
-      });
-
-      let foundStream = false;
-      await page.setRequestInterception(true);
-
-      page.on('request', request => {
-        const url = request.url();
-        if (url.includes('.m3u8?tkn=') && !foundStream) {
-          foundStream = true;
-          res.redirect(url);
-          browser.close();
-        }
-        request.continue();
-      });
-
-      // Set a timeout to close browser and return error if no stream URL is found
-      setTimeout(async () => {
-        if (!foundStream) {
-          await browser.close();
-          if (!res.headersSent) {
-            res.status(404).json({ error: 'HLS URL Bulunamadı , goflex telegramdan admine bildirebilirsiniz.' });
-          }
-        }
-      }, 10000);
-    } catch (error) {
-      console.error('Error handling Sozcu TV:', error);
-      if (!res.headersSent) {
-        res.status(500).json({ error: 'Bir hata oluştu, lütfen daha sonra tekrar deneyin.' });
-      }
-    }
   }
   else {
     res.status(404).json({ error: 'Kanal Bulunamadı , goflex telegramdan admine bildirebilirsiniz.' });
@@ -82,6 +33,6 @@ app.get('/gkn/:channel', async (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
