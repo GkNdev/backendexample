bfb1b4c6eff0fb0a272e448b66f0a8258a9e6b5c
GkNdev <kreosus98@gmail.com>
2025-04-22 13:13:55 +0300

    atv added

--- index.js
+++ index.js
@@ -28,8 +28,21 @@ app.get('/gkn/:channel', async (req, res) => {
       res.status(404).json({ error: 'HLS URL Bulunamadı , goflex telegramdan admine bildirebilirsiniz.' });
     }
   }
+  else if (channel === 'atv') {
+    try {
+      const response = await fetch('https://securevideotoken.tmgrup.com.tr/webtv/secure?468209&url=https%3A%2F%2Ftrkvz.daioncdn.net%2Fatv%2Fatv.m3u8%3Fce%3D3%26app%3Dd5eb593f-39d9-4b01-9cfd-4748e8332cf0', {
+        headers: {
+          Referer: "https://www.atv.com.tr/"
+        }
+      });
+      const data = await response.json();
+      res.redirect(data.Url)
+    } catch (error) {
+      res.send("Bir hata oluştu", error);
+    }
+  }
   else {
-    res.status(404).json({ error: 'Kanal Bulunamadı , goflex telegramdan admine bildirebilirsiniz.' });
+    res.status(404).json({ error: 'Kanal Bulunamadı , goflex/gkn telegramdan admine bildirebilirsiniz.' });
   }
 });
 
